e127e388da20f16fc028495abeb2d89539b8d0ea
Abhishek Magar <159136030+AbhiFullStackDeveloper@users.noreply.github.com>
2025-07-02 19:48:50 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "27bc1d8477f4d6c13e978dc056965dc7"; // Replace with your OpenWeatherMap API key
+const API_KEY = ""; // Replace with your OpenWeatherMap API key
 
 export const fetchWeather = async (city) => {
   const current = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
